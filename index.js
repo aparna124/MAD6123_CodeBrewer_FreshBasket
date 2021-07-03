744be6bcd32d38348bade086d14846aec19860ce
Nency Shobhashana <shobhashananency@gmail.com>
2021-07-03 13:33:42 -0400

    UI of product list and product detail page

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ import { createStackNavigator } from 'react-navigation-stack'
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import HomeScreen from './components/HomeScreen';
-import ProductList from './components/ProductList';
 import Category from './components/Category';
+import ProductList from './components/ProductList';
+import ProductDetail from './components/ProductDetail';
 
 const Navigator = createStackNavigator({
-  // ProductList: { screen: ProductList},
+  // ProductList: { screen: ProductList },
+  // ProductDetail: { screen: ProductDetail },
   SignIn: { screen: SignIn },
   SignUp: { screen: SignUp },
   Home: { screen: HomeScreen, navigationOptions: {headerShown: false} },
   ProductList: { screen: ProductList },
+  ProductDetail: { screen: ProductDetail },
 },
 
 {
